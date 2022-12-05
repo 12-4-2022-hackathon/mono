90131727898f62da7aa12e6b81359e6a77c4a795
3chospirits <3chospirits@gmail.com>
2022-12-05 15:59:24 -0500

    .

--- frontend/src/Pages/CreateJob.tsx
+++ frontend/src/Pages/CreateJob.tsx
@@ -4,13 +4,16 @@ import { useWeb3React } from "@web3-react/core"
 import { injected } from "../Util/injected"
 import styled from "styled-components"
 import { Background } from "../Components/Background"
+import { addFile } from "../Util/ipfs"
+import { createJob } from "../Util/contract"
+
 const FormContainer = styled.div`
 	text-align: center;
 	font-family: "Avenir Next";
 	color: black;
 `
 
-const Form = styled.form`
+const Form = styled.div`
 	display: inline-block;
 	margin: 0 auto;
 	padding: 20px;
@@ -47,7 +50,8 @@ const Button = styled.button`
 `
 
 export function CreateJob() {
-	const { active, activate, account } = useWeb3React()
+	const web3 = useWeb3React()
+	const { active, activate, account, library } = web3
 
 	// create form with the following information:
 	// text fill: Job bounty in eth
@@ -56,7 +60,38 @@ export function CreateJob() {
 
 	const [bounty, setBounty] = useState(0)
 	const [workers, setWorkers] = useState(0)
-	const [images, setImages] = useState(null)
+	const [image, setImage] = useState<FileList | null>(null)
+	const [success, setSuccess] = useState(false)
+
+	const filereader = new FileReader()
+
+	const onSubmit = async () => {
+		// read all the images into files
+
+		if (image == null) return
+		try {
+			let cid = await addFile(image[0])
+			console.log(cid)
+
+			await createJob(web3, bounty, "yes", cid)
+			setSuccess(true)
+		} catch (e) {
+			console.log(e)
+		}
+		// let content = await new Promise((resolve, _reject) => {
+		// 	filereader.onloadend = () => {
+		// 		resolve(filereader.result)
+		// 	}
+		// })
+
+		// const buf = Buffer.from(content as string)
+
+		// // upload content to ipfs
+		// console.log("uploading")
+		// let cid = await addFile(buf)
+		// console.log("finished")
+		// console.log(cid)
+	}
 
 	return (
 		<div>
@@ -79,12 +114,15 @@ export function CreateJob() {
 						<Input type="text" onChange={(e) => setWorkers(parseInt(e.target.value))} name="workers" />
 					</Label>
 
-                    <Label>
-                        Images{"  "}
-                        <Input type="file" onChange={(e) => setImages(e.target.files)} name="images" id="files" multiple={true}/>
-                    </Label>
+					<Label>
+						Images{"  "}
+						<Input type="file" onChange={(e) => setImage(e.target.files)} name="images" id="files" multiple={true} />
+					</Label>
 
-					<Button type="submit">Submit</Button>
+					<Button onClick={onSubmit} type="submit">
+						Submit
+					</Button>
+					{success && <div>Success!</div>}
 				</Form>
 			</FormContainer>
 		</div>
--- frontend/src/Util/ipfs.tsx
+++ frontend/src/Util/ipfs.tsx
@@ -1,9 +1,40 @@
-import ipfs from "ipfs-http-client"
+import { create } from "ipfs-http-client"
+import { Buffer } from "buffer"
 
-export const addFile = async () => {
-    
+const IPFS_CONFIG = {
+	host: "127.0.0.1",
+	// host: "api-eu1.tatum.io/v3/ipfs",
+	port: 5001,
+	protocol: "http",
 }
 
-export const getFile = async () => {
-    
+// IPFS_CONFIG using
+
+export const addFile = async (file: any) => {
+	const ipfs = create(IPFS_CONFIG)
+
+	const fileAdded = await ipfs.add(file)
+	return fileAdded.path
+}
+
+export const getFile = async (hash: string) => {
+	// get file from ipfs
+	const ipfs = create(IPFS_CONFIG)
+
+	let asyncitr = ipfs.cat(hash)
+	for await (const itr of asyncitr) {
+		return Buffer.from(itr).toString("base64")
+	}
+	throw "sdjiflsdjklfljdsl"
+	// new Promise((resolve, reject) => {
+	// 	ipfs.get(filePath, (err: any, files: any) => {
+	// 		if (err) reject(err)
+	// 		files.forEach((file) => {
+	// 			// convert base 64 to image
+	// 			const image = new Image()
+	// 			image.src = "data:image/png;base64," + file.content.toString("base64")
+	// 			resolve(image)
+	// 		})
+	// 	})
+	// })
 }
